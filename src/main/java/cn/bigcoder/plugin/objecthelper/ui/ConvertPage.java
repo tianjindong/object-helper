edcb01cd14d042a768fd5381a6a2a0ccfa42f897
Jindong.Tian <tianjindong98@qq.com>
2024-05-05 10:11:13 +0800

    新增对象转换窗口

---
+++ src/main/java/cn/bigcoder/plugin/objecthelper/ui/ConvertPage.java
@@ -0,0 +1,16 @@
+package cn.bigcoder.plugin.objecthelper.ui;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * @author: Jindong.Tian
+ * @date: 2024-03-02
+ **/
+public class ConvertPage {
+
+    private JPanel main;
+    private JLabel fromLabelVal;
+    private JLabel toLabelVal;
+    private JPanel convertPanel;
+}
